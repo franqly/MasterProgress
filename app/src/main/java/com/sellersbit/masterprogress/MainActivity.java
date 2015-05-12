4e8ab7ce9fa2756f6d443cbb6a00c87d2ea04682
Kevin Sellers <sellers.kevin@gmail.com>
2015-05-11 18:54:18 -0700

    Catchup

--- app/src/main/java/com/sellersbit/masterprogress/MainActivity.java
+++ app/src/main/java/com/sellersbit/masterprogress/MainActivity.java
@@ -1,18 +1,33 @@
 package com.sellersbit.masterprogress;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.zip.Inflater;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "ActionBarActivity";
+
+    private int savedInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +37,14 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-
-            SkillListFragment rawr = new SkillListFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.listViewContainer, rawr)
-                    .commit();
-
-
         }
+        LayoutInflater inflater = getLayoutInflater();
+        //View rootView = inflater.inflate(R.layout.skill_list_fragment, this);
+        String[] listcontents = new String[] {"one", "two", "three", "4", "t5e", "thr5ee", "thr7ee", "thr9wee"};
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.skill_list_item, listcontents);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.skill_list_item, listcontents);
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
     }
 
 
@@ -48,12 +63,66 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_enterTime:
+                Log.d(TAG, "EnterTime");
+                //Show enterTime dialog
+                Dialog dialog = createEnterTimeDialog();
+                dialog.show();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private Dialog createEnterTimeDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+
+        final View view = inflater.inflate(R.layout.alert_enter_time, null);
+        final TextView textView_time = (TextView)view.findViewById(R.id.enterTime_time);
+        final int[] timeSetInDialog = {0};
+        Button button_h = (Button)view.findViewById(R.id.enterTime_button_h);
+        button_h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Button_H: onClick");
+                timeSetInDialog[0] += 60;
+                textView_time.setText(String.valueOf(timeSetInDialog[0]));
+            }
+        });
+
+        int time = Integer.valueOf(textView_time.getText().toString());
+
+        builder.setView(view)
+                .setPositiveButton(R.string.alert_enterTime_PositiveButton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int result = Integer.valueOf(textView_time.getText().toString());
+                        saveInt(result);
+                        Log.d(TAG, "EnterTime: Confirm");
+                    }
+                })
+                .setNegativeButton(R.string.alert_enterTime_NegativeButton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "EnterTime: Cancel");
+                    }
+                });
+
+
+        Log.d(TAG, "Post: " + savedInt);
+        return builder.create();
+    }
+
+
+    private void saveInt(int a){
+        savedInt = a;
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
